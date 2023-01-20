6bae58d7d0c17b280f69dc783839347d6ff99ca0
Lionel <l.privatleblon@gmail.com>
2023-01-20 15:36:41 +0100

    Sauvegarde

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch("https://date-back-87wjznjtk-lprivatleblon-gmailcom.vercel.app/year")
+fetch("https://date-back.vercel.app/year")
   .then((response) => response.json())
   .then((data) => {
     document.querySelector("#year").textContent = data.year;
